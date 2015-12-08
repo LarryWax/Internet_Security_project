2af6717b027685a95e45793139f93fd151ad4a70
LarryWax <larrymadewax@gmail.com>
2015-12-08 15:51:27 +0100

    Aggiunto commenti, RSA 2048 al posto di 4096, aggiunta relazione

--- src/ClientIoT.java
+++ src/ClientIoT.java
@@ -9,9 +9,12 @@ public class ClientIoT {
 		
 		try{
 			DoubleCipher c = new DoubleCipher();
-			final String gatKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAhpgoWBUEsKjck+n5+IjT73YkmOjZe+rSmIWOOz1rl1NtOXtmF4xOtsYkgaBgGaDPgu6DT6MGZ0A9eBQbZXf+grd/sLlc0ZfGns2oNgjzN17EGVzXYK5pdq1wOcvC/DTfNFstJqRf3yXunNaJQV9VwK0DEI07Lu2d2sth4yXK5x/bYEUwTAKfsfo7nvw5Yt3NzI7w7RobH3TwKbSkKyYWDHqsMcFlMXKerJz3eRNBt7wMRHP4ppdM1YgewJY+oLd1fqY3deEYKH7JuZpRkQvUpqMLrKQZ+1WFXFQhBOO+7ITe408ZFs8iFVENhXEpWI5GtANIFrcHQR1dzLVA6WMhhdoJLylZqu+87SmI/rcCXz7D3ye/fXSTOJ8Uq0wmZ3V9E37BLW5lU7vCpXrB+c6RgGdgnhLXrctBN2oJEamMwPVOo0s0crDCO2sRMsNUIxIZclyjUZQ3g3iw9FBO/j5owdeUpfgiQlpUoRGPdWwTJEQJz56J6c4+sQHdPNENdZjJQ6AODZmxqBzzQ0YkuDf1ZHSZ/p9ThhEZ+HKfKU9D44yynlDbO5xi5rv1rC0VwspqL2fDfsLQvzheqMxQONLd0tjT1o/sBggpKlgQbitaRZgkc8ZQVaDts83+dcUSOHK/1jBE239S+M/85MsBIoHyNVPVQPwJ6gPHH1kZ4PB58TECAwEAAQ==";
+			final String gatKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3Eruc1Drmsr9kMh2tGDrOCx87r++2te8d3RRpl0x3+T/UfhmdY/5ogYioAAAA0W73E1i06yKWuo0fq592OqEH7oiAdj+JwALn+od0IVq/+SiXj9m0Jp3wzFl4gEWyuUllbOL8l4fVVitmENY4rzcvHsr9cdIMm8oitVMe4MyMzQMQz3VoW7bW1H0TWEFJL5aDMzWVZetrLOOZ+mZO59N5FwqFQhEsUrsQOHMcbskopLqJLk9TuZhY4UvJUeTUvHe3dplMFe95grpVmAY6qTnVPFreI350m03j2kTpgCvhVrRDtkU7qV4jGocf94EbTnIEdX26YGzi5xyVFokwBf9JwIDAQAB";
+
 			final String privKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJuYUuDustkR2vsIcF7ePeTxXs8P/PyNdiHVe0i4loBGEei5zhfIt1jyAbHUW04M3sT0rZdhOb0I3TW6yzjBJN83e+mCeXSbej13BdiaEamdXv+uyzRrLUCSefr9yChxKHD77M23UZVYBcC80bM+Fw9iq+j6OHWlWOtotk5EdBMBAgMBAAECgYBC0q51b85br3FucndgJu5KDud64hDyngZoDXk/CBeKy40ReArs6ATmLDlSGDOGV7ystbB/3P+hEdd/JdP1ce3VSNdAqXFU4tt6Iduk5QsJEgVbzLf+V1O7wd0mqNuoBjVl0O1a6RxbVF60Omz/wCIaRVETCSLOV8DxvKQIJS0oSQJBAPRnapB9zUKr9xXIPjP5rfjhwBhFqGE342KN/uGlO0NMSNhAwokY7t+bPqZh/osoi0nD54ZJjRxl1vcPxeNGK9cCQQCi+jbwgnaTUozELsn2LyOP1rZyROe+xmKl6uzLCefw/6Wd8LvdOVsQCV0PGCIpSGS0hFEJNXm2DRjw3H90gRznAkBiFE5TheilZNVXTHCJ8xy5z/6CvxF/UipSFqG+c8yAiVCCWBq5YeQan/ZYVuMLfU3IYHG9Fi8mwkeuBgpn1nUpAkA2RPmMZvUTI5bqMOZhitrdp9LDBDzjIu5wb2UAS0En2AkPKb108gdntKZ/QzsE4i1eivztAxiPTZczhUkPw3qbAkEAzhzqCVus9rcxMofXX2EFDcF0LA+saChuOK3AbSUSM2kI8v8Hz1sPmJwjjFoWq+mxbe6K1rQrxKgHLiVD9C6jpw==";
+
 			final String pubKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbmFLg7rLZEdr7CHBe3j3k8V7PD/z8jXYh1XtIuJaARhHouc4XyLdY8gGx1FtODN7E9K2XYTm9CN01uss4wSTfN3vpgnl0m3o9dwXYmhGpnV7/rss0ay1Aknn6/cgocShw++zNt1GVWAXAvNGzPhcPYqvo+jh1pVjraLZORHQTAQIDAQAB";
+
 			final PublicKey publicKey = c.genPubKeyFromStr(pubKey);
 			final PrivateKey privateKey = c.genPrivKeyFromStr(privKey);
 			final PublicKey gatewayPublicKey = c.genPubKeyFromStr(gatKey);
@@ -24,12 +27,15 @@ public class ClientIoT {
 			DataInputStream dIn = new DataInputStream(s.getInputStream());
 			DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
 		
-			//Il client genera un messaggio con la sua identita cifrata con la sua chiave privata
+			//PASSO 1			
+			//Dopo aver creato la connessione il device genera un messaggio con la sua identita, cifrata con la sua chiave privata.
+			//Il messaggio viene inviato al router.
 			final byte[] encIdMsg = c.encryptAsimmPriv(privateKey, name.getBytes(), "RSA");
 			dOut.write(encIdMsg);
 		
-			//Il client si aspetta come risposta un messaggio contenente l'ID del gateway e un seme
-			//tutto cifrato prima con la chiave pubblica dello stesso client e poi con la chiave privata del Gateway
+			//PASSO 2
+			//Il device riceve una risposta dal router. Il messaggio conterrà il seme per la generazione della chiave di sessione.
+			//Il device decripta con chiave pubblica del router e poi con la sua chiave privata.
 			byte[] dEncGatMsg = new byte[1024];
 			int len = dIn.read(dEncGatMsg);
 			dEncGatMsg = Arrays.copyOf(dEncGatMsg, len);
@@ -38,14 +44,15 @@ public class ClientIoT {
 			//Il client calcola la chiave
 			final Key simmKey = c.generateSimmKey(gatMsg);
 		
-			//Il client conclude la cerimonia con messaggio di conferma(sua chiave pubblica cifrata con chiave simmetrica)
+			//PASSO 3			
+			//Il device conclude la cerimonia con messaggio di conferma(sua chiave pubblica cifrata con chiave simmetrica)
 			final byte[] confMsg = c.encryptSimm(simmKey, c.decodePublicKey(publicKey).getBytes(), "AES");
 			dOut.write(confMsg);
 		
 			//Cerimonia conclusa
 			System.out.println(name+" authenticated with Gateway");
 			
-
+			//Da questo momento in poi il device comunicherà con la classe IoTManager utilizzando la chiave di sessione.
 			while(true){
 				byte[] encRequest = new byte[2048];
 				len = dIn.read(encRequest);
--- src/Server.java
+++ src/Server.java
@@ -13,7 +13,6 @@ public class Server {
 	final static Object responseMonitor = new Object();
 	
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InterruptedException{
-		// TODO Auto-generated method stub
 	
 		Thread serverIoT = new ServerIoT();
 		
@@ -36,7 +35,8 @@ public class Server {
 			}
 		}	
 	}
-	
+	/*Questa classe sta in ascolto di nuovi device.
+	Ogni qualvolta arriva una richiesta inizia la cerimonia per l'autenticazione e lo scambio del segreto per generare la chiave di sessione*/
 	static class ServerIoT extends Thread {
 		
 		final Whitelist W;
@@ -47,14 +47,20 @@ public class Server {
 		
 		public ServerIoT() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException{
 			
+			//La chiavi sono fornite come stringhe, in seguito verranno trasformate in oggetti chiave.
+			//Non è il modo più elegante, ma è quello più rapido da implementare.
+
+			//Di seguito abbiamo le chiavi e gli identificatori dei device che saranno salvati nella whitelist. Le chiavi sono state generate con RSA 1024.
 			String[] key = {"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbmFLg7rLZEdr7CHBe3j3k8V7PD/z8jXYh1XtIuJaARhHouc4XyLdY8gGx1FtODN7E9K2XYTm9CN01uss4wSTfN3vpgnl0m3o9dwXYmhGpnV7/rss0ay1Aknn6/cgocShw++zNt1GVWAXAvNGzPhcPYqvo+jh1pVjraLZORHQTAQIDAQAB","MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCGNymNF3th2fsrCYJ6bbndf+YibapiIYV/Jx55Mkegm+EsBQOPmTjh5pXgoUqFuNR8co15beTVxK17OcQ4BTBzU7ui8ssUeCRvozYHQWlMFgnSztzi6+yH740UVlCVL7USochLLQRkgMJ4oCAhXT1sSOSGhoiW3bmlnj321FJY0QIDAQAB"};
 			String[] ID = {"ALARM","LIGHT"};
 			W = new Whitelist(ID, key);
-			
-			String privateKeyStr = "MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQCGmChYFQSwqNyT6fn4iNPvdiSY6Nl76tKYhY47PWuXU205e2YXjE62xiSBoGAZoM+C7oNPowZnQD14FBtld/6Ct3+wuVzRl8aezag2CPM3XsQZXNdgrml2rXA5y8L8NN80Wy0mpF/fJe6c1olBX1XArQMQjTsu7Z3ay2HjJcrnH9tgRTBMAp+x+jue/Dli3c3MjvDtGhsfdPAptKQrJhYMeqwxwWUxcp6snPd5E0G3vAxEc/iml0zViB7Alj6gt3V+pjd14Rgofsm5mlGRC9SmowuspBn7VYVcVCEE477shN7jTxkWzyIVUQ2FcSlYjka0A0gWtwdBHV3MtUDpYyGF2gkvKVmq77ztKYj+twJfPsPfJ799dJM4nxSrTCZndX0TfsEtbmVTu8KlesH5zpGAZ2CeEtety0E3agkRqYzA9U6jSzRysMI7axEyw1QjEhlyXKNRlDeDeLD0UE7+PmjB15Sl+CJCWlShEY91bBMkRAnPnonpzj6xAd080Q11mMlDoA4NmbGoHPNDRiS4N/VkdJn+n1OGERn4cp8pT0PjjLKeUNs7nGLmu/WsLRXCymovZ8N+wtC/OF6ozFA40t3S2NPWj+wGCCkqWBBuK1pFmCRzxlBVoO2zzf51xRI4cr/WMETbf1L4z/zkywEigfI1U9VA/AnqA8cfWRng8HnxMQIDAQABAoICAFOuyO6y6r6XzsUcZXH0SKkK8IgtxgK4poV0DFGix4FHupJRudEMSGh1W39I4lcs17hVaUkbWEDb/RvqjHoTGCfXxUlwhlg+IJzdyycFe4rCnfQS+EpBRosmHK+ik7HRgbGAutraukz4vZm//YiqqJtqNoyU8uYjkNDXQR5EghofgXsiqXuo6fdzWJxpv5hLxcfYW81fPNvbDfMwQB4fyPerUeKBltQeqq8xzN/3bBW7knkBDekuCvR8dHpTL88nIaND3xmogSe0ZOHEI9Cl5WpR11FYmlrc+lTEbd8nZq6Rki8MH9JBqLRON7Tu/cD0fNsxayHMXHNMueQsEZbKLq2tP7vyHGU1rvbFsMN1Ejjb5Smu5GdTgdWbqWbEVfj4wsdcJOtV3DWquu+lYAoA4nqJ0awyuuK5ryEjT3u4AGfJ5QV/Ub71sIeUwmhHLpDy9YAA0uD1+ACRxeni81eyDL9LYvdgLP9ve8M2wwTo7RWuM4Q4LPMoKFVqbtf0GMFZ7z+dHZZle6/nKPIKoj50NQVtlBGRa3yU4Z+P9dUJ5/e8CLColy7DLfre7HYHKGf0tuif5ONvRf6yrBQkR17wdk0JvZioid48dd6ocaikRHXgNXObmeNZP0bi0C2ddEc1aPztH0Wkivf1PTDg5pFmnptv63sHGiVQYLW0p78dJa59AoIBAQDNIR3KKLYeiMCvvtF040cq0+BxMmLwVNrARMmggZvW1CE566rCQ0ReeJE356Q0KqpQ+a3O3RzgRNQza8KaBZTSSBuGEC/dgQB+KUHqZvyMQ1Dt8FRrGmu67zCfOfNXsW5Ys0svt3S8RowRAcIvSaClCYmMfkcVOvr9koSiJ2lmQQmcVfdtPwGd6rsN4BFc7I/z64zx4K+4X4vjZuaJCfSPNcUvcSlY/B5MWYHEmkcqjUezcj9nghcYPsYN3l3R8LQRHLM85ByhcYyfHkUYadqFcw7WYjbNNftTfOCArnNc9wQ6A8b6atGely7JO81htzEs/TbfwB8ZG7VOpIkzscR7AoIBAQCn+QrbkS+cJUMYdIsFm9l4Z044ToBtqApdQDAXqdA1Mb6Wjqhh4yX4R3HAaf3XSlKm89Vnk2utTBjPqakOj0jfMvCp1euwKB935C78Thj5XACCWiQGsokGhqpcYLaaqvqBzNMIRZy6mistPAI2AhktpD+wcW6Buu3O8BOybAECiPiHKFeYOf9CPj9oAiYvBKxaRbegL35K6ukynFgC6re1feynwfhQRrwXVIMYwkGo547xq7QMy9oJgIblMwcDyIoU5N32ekH8HlZHvIso1Wbg1oA6k+bNuFXiTt3dn+9RtRPxHR6fdB2ELuGgflFOCzEx9vnHNqjHlkXDLbpep/9DAoIBAQCmyk6WmhO7MCa8v8M/761mPXE5mWapU9WqkEQBpmE4hLOfjOgDnP5yckZh1o/FewuRW/LfSRq5oD2KsgxBgFhJwyfeEjtZOBNWgeV+2s5uni2nyspZa9o83VADp/MdUsAOj3qLCfh5PkOVcICcQpnEl8Sn3+WcdaCvnfsKflFNO4S9AYl14s1Fwh7mYVK84/0IsHaiFbfLiqUL+hdpRhb8exQiRzj/icB5BC5n1aoYB1hNqI+1cimlJSWVEUHn+nfvkg6LbE97VgCpQMud1jRAXhWqa6Fzqnj4oYlKGI7lAFUHdxrLcx+OfA8mwOF2mqX5lsqPlH+0wrrhfxewWNMRAoIBADFOs9c8SUOG1KlLHLwqwa3MdXOl56FI4LH686MeoBXdYlsDyX3ilfTJ1CRteKxIDvcfQJfE+KjIW8gpv4yclwGeDHNubl8piANDol9B9abNUQDywPyOUBbwVPYFFdVVN2/+1jvpY1aLhv0iTmvYNdfmDSQYSB4g50oFw2JuveUmm/3g48W10K57Y1V+vAK1VnqxWHPXPTV0u7u1lUhV6lOGgX/FOsUl3nTixBDyhR+1yYo7owDWnkyHxBFn5zk/FvmHLXj67n+50A4C1L7J1gdOMBoB7JCa+esSx6IVHsVXaGEw8RpRlZCFyom4StmkA27tVukZrHXaMORYegrrOc0CggEANWqOIIkyeqADgpZE4NgFqn3gdbECpo5fgBPjLqEwFlo+ZGuXgzVhfF7VmmpcNfGF+ojTPDrrNWhwfNBggL0qP9E6bIlGQ0gu5SB3V1UEqek0XG97xcf4g//2VYTyAZKqbX1j2hkAWXPXU+1ETETuJMUPp5Mv+87tgWVq+HW49Hs4DOCyBFfENXLxh3TFYKD/BTGdxx9nQOntaX4iGWFPLBYS6Ovm/+5HAyi10+l83qkuHboJywITvngj7undIFGLxKG2Aeq/w25KtiNuxQ6XChU8uzwUoKiZSONyhomOXX1b5fxd+0zcDyIJDJ6NraO9NVL/HA2r+yfkAsG11unauA==";
 
-			String publicKeyStr = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAhpgoWBUEsKjck+n5+IjT73YkmOjZe+rSmIWOOz1rl1NtOXtmF4xOtsYkgaBgGaDPgu6DT6MGZ0A9eBQbZXf+grd/sLlc0ZfGns2oNgjzN17EGVzXYK5pdq1wOcvC/DTfNFstJqRf3yXunNaJQV9VwK0DEI07Lu2d2sth4yXK5x/bYEUwTAKfsfo7nvw5Yt3NzI7w7RobH3TwKbSkKyYWDHqsMcFlMXKerJz3eRNBt7wMRHP4ppdM1YgewJY+oLd1fqY3deEYKH7JuZpRkQvUpqMLrKQZ+1WFXFQhBOO+7ITe408ZFs8iFVENhXEpWI5GtANIFrcHQR1dzLVA6WMhhdoJLylZqu+87SmI/rcCXz7D3ye/fXSTOJ8Uq0wmZ3V9E37BLW5lU7vCpXrB+c6RgGdgnhLXrctBN2oJEamMwPVOo0s0crDCO2sRMsNUIxIZclyjUZQ3g3iw9FBO/j5owdeUpfgiQlpUoRGPdWwTJEQJz56J6c4+sQHdPNENdZjJQ6AODZmxqBzzQ0YkuDf1ZHSZ/p9ThhEZ+HKfKU9D44yynlDbO5xi5rv1rC0VwspqL2fDfsLQvzheqMxQONLd0tjT1o/sBggpKlgQbitaRZgkc8ZQVaDts83+dcUSOHK/1jBE239S+M/85MsBIoHyNVPVQPwJ6gPHH1kZ4PB58TECAwEAAQ==";
+			//Di seguito abbiamo chiave pubblica e privata del server (router), sono generate con RSA 2048.			
+			String privateKeyStr = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDcSu5zUOuayv2QyHa0YOs4LHzuv77a17x3dFGmXTHf5P9R+GZ1j/miBiKgAAADRbvcTWLTrIpa6jR+rn3Y6oQfuiIB2P4nAAuf6h3QhWr/5KJeP2bQmnfDMWXiARbK5SWVs4vyXh9VWK2YQ1jivNy8eyv1x0gybyiK1Ux7gzIzNAxDPdWhbttbUfRNYQUkvloMzNZVl62ss45n6Zk7n03kXCoVCESxSuxA4cxxuySikuokuT1O5mFjhS8lR5NS8d7d2mUwV73mCulWYBjqpOdU8Wt4jfnSbTePaROmAK+FWtEO2RTupXiMahx/3gRtOcgR1fbpgbOLnHJUWiTAF/0nAgMBAAECggEBAJsHXKHou7XWzvWaLfepEUAaBt3aMQ0/r9F9GYzznwrK1GeyYSCw7uP4X1usRFYJODjEeR0wGrn6gnD4iXYjLFJLom0ccVeu739duheqHO/yzdV31vpNCqViWSngzFKDSpz8mys40+IzIw+5rY77jHhUUk6ILSXZFnYFv+pYHPbshl3/ZUvTaBvcdFI1SXstaPsNJuazPXUGYMrS8GE9z1lmflFzD4LIRhk9r44Oq42AZ3NAm9l5sOym5ETfV1SYK1qALGOs5k1igzt8UwzsMu5xdfzKSIinXwC5iaidEbkbdi/hd2TzD/BrNRB+HG2XdGfMZU4J6c0ZtNivhlnzS3ECgYEA+aqd8A6Zj7wiATCh8Ttueuu9btzSTds7Jpz23oEppInGwVRDONzaNnSZSA1RRx1b08dnKZN3mPBz07lsRpm34qs9nkHeo/HXQLiY8HXJEWjkAM0XouzIh4cJjIvLTp0XqoRQYTDMDa0R+vIk4pfEL//a1BKHfDZJsClTYyvbxMkCgYEA4eGOMyRnwof891rWeET9/O7yzsOWQ+zHwKVa9pt/SeBjPokLgvJrnBtbyj+Is37mGTxxqNaBcpAZOnOzqsu2fFuUHmS4xzb/uq2T1YSN8z97RxPsYfLcH6CYh8udAErRarH9XQtRFAqdtf7TvgTLsI13eCZ7tRaGvYO0344nWm8CgYAaH0DhIhXtFQkVyMnY3JgFom6yj8YtQTfiJWArDaaWwZebGL7jzy/BrNdw03vije7SrLVdg0FrLhVgXLE4fdLgk3wh99BcpOwZqHbQCfLcsHXSo6LKeC3PCFD2dJX0Brm4LJngYWOoNqh/8h/r2Wc8gm+Z6spkM6XoGZrQ8IDRCQKBgDgTlQfhS9khhY2rAX+OMTfv4luudsmaKOUMqA59ABbJHBpUT4VMxaZVrG4DY96b7Gfrd5Qg3LYiTAEU5E/MjxtEaPs6qhza7JXdrcveQpdW9TXqHk8ayccYSYW86Nc/u8mvMmU8//3nvMuH3e1Yp3DHTheIBkIou36lCexOEDsFAoGBAIomJlZ5YkIxHGW7fLtUG0AcsFDh2k982IzvAUeicvmkQ3a2FQ1eQMQebB65Y9ggpJZYK95twzmDKcYjWXqMaZB97AQ/ARpoIFcPjxl6fx8ivpC1L5b02hnBs0Co/ts9QjZQ7BxxFaRjegI8vHPYlP1tg/Mf/QPrCfo4e/t4zLUm";
+
+			String publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3Eruc1Drmsr9kMh2tGDrOCx87r++2te8d3RRpl0x3+T/UfhmdY/5ogYioAAAA0W73E1i06yKWuo0fq592OqEH7oiAdj+JwALn+od0IVq/+SiXj9m0Jp3wzFl4gEWyuUllbOL8l4fVVitmENY4rzcvHsr9cdIMm8oitVMe4MyMzQMQz3VoW7bW1H0TWEFJL5aDMzWVZetrLOOZ+mZO59N5FwqFQhEsUrsQOHMcbskopLqJLk9TuZhY4UvJUeTUvHe3dplMFe95grpVmAY6qTnVPFreI350m03j2kTpgCvhVrRDtkU7qV4jGocf94EbTnIEdX26YGzi5xyVFokwBf9JwIDAQAB";
 			
+			//L'oggetto DoubleCipher è una classe che implenta diversi metodi per la generazione delle chiavi e l'encrypt/decrypt.
 			c = new DoubleCipher();
 			privateKey = c.genPrivKeyFromStr(privateKeyStr);
 			publicKey = c.genPubKeyFromStr(publicKeyStr);
@@ -67,17 +73,24 @@ public class Server {
 			
 			while(true){
 				try{
-					//Listen for someone
+					
 					Socket clientIoT = listenerIoT.accept();
 				
 					//Inizio della cerimonia
 					//Il server si aspetta un messaggio che è possibile decifrare con le chiavi pubbliche della whitelist
 					DataInputStream dIn = new DataInputStream(clientIoT.getInputStream());
 					DataOutputStream dOut = new DataOutputStream(clientIoT.getOutputStream());
-				
+					
+					//PASSO 1
+					//Ricezione del messsaggio criptato come array di byte.
+					//Dopo la ricezione viene eseguito un ridimensionamento dell'array, perchè la decryption deve ottenere in input il corretto numero di byte.
 					byte [] encCliID = new byte[1024];
 					int len = dIn.read(encCliID);
 					encCliID = Arrays.copyOf(encCliID, len);
+
+					/*Qui il server prova a decriptare il messaggio con le chiavi della whitelist, una alla volta.
+					Se la chiave utilizzata non è corretta la decryption genera un eccezzione 
+					che viene gestita dal try/catch per non fermare l'esecuzione del for*/
 					int i;
 						for(i=0; i<W.clientPublicKey.length; i++){
 							try{
@@ -95,7 +108,8 @@ public class Server {
 							clientIoT.close();
 						}
 					
-						//Gateway manda la sua identita e il segreto per la chiave simmetrica tutto cifrato prima
+						//PASSO 2
+						//Il router manda la sua identita e il segreto per la chiave simmetrica tutto cifrato prima
 						//con la chiave pubblica del client e poi con la sua chiave privata
 						String seed = "INTERNET_SECURITY";
 						final byte[] encGatMsg = c.doubleEncrypt(seed, W.clientPublicKey[i], privateKey);
@@ -104,19 +118,25 @@ public class Server {
 						//Gateway calcola la chiave simmetrica
 						final Key simmKey = c.generateSimmKey(seed);
 				
+						//PASSO 3
 						//Il client risponde con la sua chiave pubblica criptata con la chiave simmetrica
 						byte[] encCliKey = new byte[1024];
 						len = dIn.read(encCliKey);
 						encCliKey = Arrays.copyOf(encCliKey, len);
 						String cliKeyStr = new String(c.decryptSimm(simmKey, encCliKey, "AES"));
-					
+						
+						//Controllo sul contenuto del messaggio. Se la chiave di sessione è corretta, ma la chiave pubblica contenuta nel messaggio
+						//non è la stessa la cerimonia viene interrotta.
+						//Viene interrotta l'esecuzione del thread, perchè la chiave potrebbe essere stata compromessa		
 						if(!(cliKeyStr.equals(c.decodePublicKey(W.clientPublicKey[i])))){
 							System.out.println("\n"+cliKeyStr+"\n");
 							System.out.println("\n"+c.decodePublicKey(W.clientPublicKey[i])+"\n");
 							clientIoT.close();
 							break;
 						}
-				
+						
+						//Il device è stato autenticato con successo, viene lanciato un thread che si occuperà dello scambio dei messaggi
+						//utilizzando la chiave di sessione.
 						System.out.println("\n"+W.clientID[i]+" IoT client authenticated!");
 						Thread t = new IoTManager(W.clientID[i], clientIoT, simmKey);		
 						t.start();
@@ -128,6 +148,8 @@ public class Server {
 		}
 	}
 	
+	//Questa classe si occupa di scambiare messaggi con il device già autenticato usando la chiave simmetrica generata durante il protocollo.
+	//Le richieste arrivano da un client remote e vengono passate a questa classe tramite buffer condivisi, per questo è stato necessario implementare dei monitor
 	static class IoTManager extends Thread {
 
 		private Socket cliSocket;
@@ -173,13 +195,16 @@ public class Server {
 				try {
 					cliSocket.close();
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		}
 	}
 	
+	//La seguente classe si occuppa di scambiare messaggi con i client remoti, 
+	//ogni volta che riceve una richiesta dal client, la inserisce nel buffer condiviso e risveglia i thread che gestiscono i device.
+	//il thread chiama la wait sul buffer di risposta. 
+	//Appena i gestori dei device ottengono la risposta, la mettono nel buffer condiviso e notificano i gestori dei client che si risvegliano.
 	static class ClientManager extends Thread{
 		
 		private Socket client;
